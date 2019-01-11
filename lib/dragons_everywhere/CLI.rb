35f8a4e9bfd84389dc9e1d4ea76c349e96dd9749
Andrew Trent <andrewtrentdesign@gmail.com>
2019-01-11 16:28:34 -0500

    add first level of CLI option methods

--- lib/dragons_everywhere/CLI.rb
+++ lib/dragons_everywhere/CLI.rb
@@ -2,6 +2,7 @@ module DragonsEverywhere::CLI
 
   def self.go
     self.greet
+    self.dragon_realm_get
   end
 
   def self.greet
@@ -10,7 +11,25 @@ module DragonsEverywhere::CLI
       1. Real
       2. Fake
       3. Metaphorical
+      4. No dragons please
     HEREDOC
   end
+
+  def self.dragon_realm_get
+    input = gets.strip.to_i
+    if input = 1
+      self.menu_real_dragons
+    elsif input = 2
+      self.menu_fake_dragons
+    elsif input = 3
+      self.menu_metaphorical_dragons
+    elsif input = 4
+      self.menu_no_dragons
+    else
+      puts "I don't understand. What other choice is there?"
+    end
+  end
+
+  def 
 
 end
